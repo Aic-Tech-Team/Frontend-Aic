cdf313643859b16ec54a96a89c1c65eddb2bdbe3
Anahita <150595572+anahita4257@users.noreply.github.com>
2026-07-21 13:47:30 +0330

    feat(landing): implement the secend part for CTA section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import { Hero } from "@/components/sections/hero";
 import { EventsSection } from "@/components/sections/EventsSection";
 import { ActivitiesSection } from "@/components/sections/ActivitiesSection";
 import { BlogSection } from "@/components/sections/BlogSection";
+import { CtaSection } from "@/components/sections/CtaSection";
 
 
 export default async function HomePage({
@@ -19,6 +20,7 @@ export default async function HomePage({
       <EventsSection />
       <ActivitiesSection />
       <BlogSection />
+      <CtaSection />
     </>
   );
 }
